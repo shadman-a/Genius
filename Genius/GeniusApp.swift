2d383e11efa124b22fc7b079f4f7752b7e4c4594
shadman-a <ahmedshadman966@gmail.com>
2025-06-10 12:34:03 -0400

    Initial Commit

---
+++ Genius/GeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeniusApp.swift
+//  Genius
+//
+//  Created by Shadman Ahmed on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
